2c36fabf6d7c29113501ea9b9a2ab0dbe426e42d
Chambani <tshambanimabona@gmail.com>
2023-05-08 06:52:12 -0700

    ./1-memcpy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies @n bytes from memory area @src
+ * to memory area @dest
+ *
+ * @n: function copies
+ *
+ * @src: bytes from memory area
+ *
+ * @dest: to memory area
+ *
+ * Return: a pointer @dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < 0);
+	{
+		dest [i] = src[i];
+		i++;
+	}
+	return (dest);
+}
